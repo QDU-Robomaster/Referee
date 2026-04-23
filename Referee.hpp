0e12df11cd95f610a59d38e05d39f18d82dd7897
Gcgc4514 <you@example.com>
2026-04-24 02:31:54 +0800

    launcher pack更新

--- Referee.hpp
+++ Referee.hpp
@@ -984,8 +984,9 @@ class Referee : public LibXR::Application {
    */
   struct [[gnu::packed]] LauncherPack {
     RobotStatus rs; /* 热量上限和冷却速率 */
-    // PowerHeat ph;
-    uint16_t launcher_id1_17_heat; /* 第1个17mm发射机构的射击热量 */
+    RobotBuff rb; /* 冷却增益 */
+    LauncherData ld;  /* 射速 */
+    DartClient dc;  /* 飞镖发射 */
   };
 
   /**
@@ -1767,12 +1768,11 @@ class Referee : public LibXR::Application {
     this->cp_.rs = this->data_.robot_status;
     this->cp_.power_buffer = this->data_.power_heat.chassis_pwr_buff;
     this->chassispack_topic_.Publish(this->cp_);
-    this->lp_.rs.shooter_cooling_value =
-        this->data_.robot_status.shooter_cooling_value;
-    this->lp_.rs.shooter_heat_limit =
-        this->data_.robot_status.shooter_heat_limit;
-    this->lp_.launcher_id1_17_heat =
-        this->data_.power_heat.launcher_id1_17_heat;
+    this->lp_.rs = this->data_.robot_status;
+    this->lp_.rb = this->data_.robot_buff;
+    this->lp_.ld = this->data_.launcher_data;
+    this->lp_.dc = this->data_.dart_client;
+
     this->launcherpack_topic_.Publish(this->lp_);
     this->sp_.rs = this->data_.robot_status;
     this->sp_.gs = this->data_.game_status;
